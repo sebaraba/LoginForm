e8869328dd89fefa425f26ed8d3bb39fe647b7d8
sebaraba <rabasebastians@gmail.com>
2020-05-10 00:25:17 +0100

    Add RegisterForm and clean up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+
+import React, {useState} from 'react';
+import './App.css';
 import Header from './componenets/Header/Headers.js';
+import RegistrationForm from './componenets/RegisterForm/RegisterForm.js';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
+  const [title, updateTitle] = useState(null);
+  const [errorMessage, updateErrorMessage] = useState(null);
   return (
-    <div className="App">
-      <Header />
+    <Router>
+      <div className="App">
+        <Header/>
+          <div className="container d-flex align-items-center flex-column">
+            <Switch>
+            <Route path="/" exact={true}>
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            <Route path="/register">
+              <RegistrationForm showError={updateErrorMessage} updateTitle={updateTitle}/>
+            </Route>
+            </Switch>
+        </div>
     </div>
+    </Router>
   )
 }
 
